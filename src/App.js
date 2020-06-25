ea3445fbdef93711aed94ee288a07fd46234b1f7
shraddhansupanda <pandataj6@gmail.com>
2020-06-26 00:59:01 +0530

    From for Authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { Button } from 'semantic-ui-react'
+
+import FormExampleCaptureValues from './inputform';
+import { Header } from 'semantic-ui-react'
+
+
+function App() {    
   return (
     <div className="App">
+      <Header as='h1'>Enter your name and Email ID</Header>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <FormExampleCaptureValues />   
       </header>
     </div>
   );
