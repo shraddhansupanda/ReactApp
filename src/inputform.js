4def0703ae3d2e3c5d3982c0661cf87303f86a46
shraddhansu panda <47125559+shraddhansupanda@users.noreply.github.com>
2021-08-20 12:29:23 +0530

    Update inputform.js

--- src/inputform.js
+++ src/inputform.js
@@ -55,7 +55,7 @@ class Authentication extends Component {
                     value={email}
                     onChange={this.handleChange}
                   />
-                  <Form.Button type="submit"  content='Submit' />
+                  <Form.Button type="submit"  content='Submit01' />
                 </Form.Group>
               </Form>
               :<Booking name ={name} email= {email} />
